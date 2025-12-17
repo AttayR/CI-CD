3b4936698c44d382f5255aeada33447b8c4d59cf
Attay e Rasool <attayerasool@APPLEs-MacBook-Pro-2.local>
2025-12-17 11:11:17 +0500

    update target sdk

--- android/app/src/main/java/com/cicdhose/app/MainActivity.kt
+++ android/app/src/main/java/com/cicdhose/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.cicdhose.app
+package com.authenticationapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/cicdhose/app/MainApplication.kt
+++ android/app/src/main/java/com/cicdhose/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.cicdhose.app
+package com.authenticationapp
 
 import android.app.Application
 import com.facebook.react.PackageList
